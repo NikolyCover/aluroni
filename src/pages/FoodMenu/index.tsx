2e8635fbe371110189d9bb41485aa0ffd6c729d8
Nikoly Cover Pereira <nikoly.cp@pti.org.br>
2022-09-27 09:18:22 -0300

    refactoring routes

--- src/pages/FoodMenu/index.tsx
+++ src/pages/FoodMenu/index.tsx
@@ -1,5 +1,4 @@
 import styles from './FoodMenu.module.scss'
-import {ReactComponent as Logo} from 'assets/logo.svg'
 import Searcher from './Searcher'
 import { useState } from 'react'
 import Filters from './Filters'
@@ -12,24 +11,14 @@ export default function FoodMenu() {
   const [orderer, setOrderer] = useState('')
 
   return (
-    <main>
-      <nav className={styles.menu}>
-        <Logo/>
-      </nav>
-      <header className={styles.header}>
-        <div className={styles.header__text}>
-                    A casa do código e da massa
-        </div>
-      </header>
-      <section className={styles.foodMenu}>
-        <h3 className={styles.foodMenu__title}>Cardápio</h3>
-        <Searcher search={search} setSearch={setSearch} />
-        <div className={styles.foodMenu__filters}>
-          <Filters filter={filter} setFilter={setFilter}/>
-          <Orderer orderer={orderer} setOrderer={setOrderer} />
-        </div>
-        <Items search={search} filter={filter} orderer={orderer}/>
-      </section>
-    </main>
+    <section className={styles.foodMenu}>
+      <h3 className={styles.foodMenu__title}>Cardápio</h3>
+      <Searcher search={search} setSearch={setSearch} />
+      <div className={styles.foodMenu__filters}>
+        <Filters filter={filter} setFilter={setFilter}/>
+        <Orderer orderer={orderer} setOrderer={setOrderer} />
+      </div>
+      <Items search={search} filter={filter} orderer={orderer}/>
+    </section>
   )
 }
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,21 @@
+import DefaultPage from 'components/DefaultPage'
+import Menu from 'components/Menu'
 import FoodMenu from 'pages/FoodMenu'
 import Home from 'pages/Home'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 export default function AppRouter() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/foodMenu' element={<FoodMenu/>} />
-      </Routes>
-    </Router>
+    <main>
+      <Router>
+        <Menu />
+        <Routes>
+          <Route path='/' element={<DefaultPage />}>
+            <Route index element={<Home />} />
+            <Route path='foodMenu' element={<FoodMenu/>} />
+          </Route>
+        </Routes>
+      </Router>
+    </main>
   )
 }
